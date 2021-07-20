81b400219baa50e4b4ecb9dbe913312391ea159c
ereminmaksim <eremin1988ma21@gmail.com>
2021-07-20 15:51:46 +0300

    check lesson_3 v.0.1

--- src/components/FuncionComponents/AddMessageFunction.js
+++ src/components/FuncionComponents/AddMessageFunction.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ListItemText, TextField} from "@material-ui/core";
+import { ListItemText, TextField } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
@@ -10,7 +10,7 @@ import Avatar from "@material-ui/core/Avatar";
 import Fab from "@material-ui/core/Fab";
 import SendIcon from "@material-ui/icons/Send";
 import style from '../../style/AddMessage.module.css';
-import {useStyles} from "../../const/const";
+import { useStyles } from "../../const/const";
 
 
 const AddMessageFunction = (props) => {
@@ -44,42 +44,43 @@ const AddMessageFunction = (props) => {
                             <ListItem button>
                                 <ListItemIcon>
                                     <Avatar alt="#"
-                                            src="https://yt3.ggpht.com/a/AATXAJxnmWzcFMTNjX43-9r3Ujps9JoAJ6_-mbxbO2rC=s900-c-k-c0xffffffff-no-rj-mo"/>
+                                        src="https://yt3.ggpht.com/a/AATXAJxnmWzcFMTNjX43-9r3Ujps9JoAJ6_-mbxbO2rC=s900-c-k-c0xffffffff-no-rj-mo" />
                                 </ListItemIcon>
                                 <span className={style.textElement}> Chat 1 </span>
                             </ListItem>
                             <ListItem button>
                                 <ListItemIcon>
                                     <Avatar alt="#"
-                                            src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/053e22db-ee81-4773-9d01-2a8891151aeb/dbqg8wi-9e5db056-bc4f-4dea-b9b7-bb000d206e26.png/v1/fill/w_894,h_894,q_75,strp/creepy_discord_icon___logo_remix_by_treetoadart-dbqg8wi.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl0sIm9iaiI6W1t7InBhdGgiOiIvZi8wNTNlMjJkYi1lZTgxLTQ3NzMtOWQwMS0yYTg4OTExNTFhZWIvZGJxZzh3aS05ZTVkYjA1Ni1iYzRmLTRkZWEtYjliNy1iYjAwMGQyMDZlMjYucG5nIiwid2lkdGgiOiI8PTg5NCIsImhlaWdodCI6Ijw9ODk0In1dXX0.kzhAG8lkmSYnMEWs-8hD4uPc-RTrU2OtGmIiUFECd5A"/>
+                                        src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/053e22db-ee81-4773-9d01-2a8891151aeb/dbqg8wi-9e5db056-bc4f-4dea-b9b7-bb000d206e26.png/v1/fill/w_894,h_894,q_75,strp/creepy_discord_icon___logo_remix_by_treetoadart-dbqg8wi.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl0sIm9iaiI6W1t7InBhdGgiOiIvZi8wNTNlMjJkYi1lZTgxLTQ3NzMtOWQwMS0yYTg4OTExNTFhZWIvZGJxZzh3aS05ZTVkYjA1Ni1iYzRmLTRkZWEtYjliNy1iYjAwMGQyMDZlMjYucG5nIiwid2lkdGgiOiI8PTg5NCIsImhlaWdodCI6Ijw9ODk0In1dXX0.kzhAG8lkmSYnMEWs-8hD4uPc-RTrU2OtGmIiUFECd5A" />
                                 </ListItemIcon>
                                 <span className={style.textElement}> Chat 2 </span>
                             </ListItem>
                             <ListItem button>
                                 <ListItemIcon>
                                     <Avatar alt="#"
-                                            src="https://yt3.ggpht.com/a/AATXAJyouE5pCIcyPNIovM6ZQbopYQ_uxu__JH74ktFF=s900-c-k-c0x00ffffff-no-rj"/>
+                                        src="https://yt3.ggpht.com/a/AATXAJyouE5pCIcyPNIovM6ZQbopYQ_uxu__JH74ktFF=s900-c-k-c0x00ffffff-no-rj" />
                                 </ListItemIcon>
                                 <span className={style.textElement}> Chat 3 </span>
                             </ListItem>
                         </List>
                     </Grid>
                     <Grid item xs={9}>
-                        <List style={{'overflowY': "scroll", 'height': '328px'}}>
+                        <List style={{ 'overflowY': "scroll", 'height': '328px' }}>
                             {props.listMessage}
                         </List>
-                        <Grid container style={{padding: '20px'}}>
+                        <Grid container style={{ padding: '20px' }}>
                             <form className={style.formAction} action="#" onSubmit={handleSubmit}>
                                 <TextField value={newText}
-                                           onChange={handleChange}
-                                           id="outlined-basic-email"
-                                           label="Enter a message"
-                                           required
-                                           fullWidth/>
+                                    onChange={handleChange}
+                                    id="outlined-basic-email"
+                                    label="Enter a message"
+                                    required
+                                    autoFocus={true}
+                                    fullWidth />
                                 <Fab className={classes.btn}
-                                     type="submit"
-                                     color="primary"
-                                     aria-label="add"><SendIcon/></Fab>
+                                    type="submit"
+                                    color="primary"
+                                    aria-label="add"><SendIcon /></Fab>
                             </form>
                         </Grid>
                     </Grid>
